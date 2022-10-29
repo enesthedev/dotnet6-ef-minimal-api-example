fd65fd5675873247fb992eba06c7f923e51eea8c
Enes Bayraktar <enes.bayraktar@outlook.com.tr>
2022-10-29 12:24:14 +0300

    Add: Swagger support

--- Program.cs
+++ Program.cs
@@ -1,5 +1,25 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "PizzaStore EF API",
+        Description = "Making the Pizzas you love",
+        Version = "v1"
+    });
+});
+
 var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "PizzaStore EF API v1");
+});
 
 app.MapGet("/", () => "Hello World!");
 
