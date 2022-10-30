7d09831200e0b1a5e80d9a7a9cf3c2582d3a25dc
Enes Bayraktar <enes.bayraktar@outlook.com.tr>
2022-10-30 13:29:34 +0300

    Add: CRUD operations of Pizza

--- Program.cs
+++ Program.cs
@@ -1,6 +1,10 @@
+using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
+using PizzaStoreEF;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<PizzaDb>(options => options.UseInMemoryDatabase("items"));
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
@@ -21,6 +25,44 @@ app.UseSwaggerUI(c =>
     c.SwaggerEndpoint("/swagger/v1/swagger.json", "PizzaStore EF API v1");
 });
 
+app.MapPost("/pizzas", async (PizzaDb db, Pizza pizza) =>
+{
+    await db.Pizzas.AddAsync(pizza);
+    await db.SaveChangesAsync();
+    return Results.Created($"/pizza/{pizza.Id}", pizza);
+});
+
+app.MapGet("/pizzas", async (PizzaDb db) => await db.Pizzas.ToListAsync());
+app.MapGet("/pizza/{id}", async (PizzaDb db, int id) => await db.Pizzas.FindAsync(id));
 app.MapGet("/", () => "Hello World!");
+
+app.MapPut("/pizza/{id}", async (PizzaDb db, Pizza updatePizza, int id) =>
+{
+     var pizza = await db.Pizzas.FindAsync(id);
+     if (pizza is null)
+     {
+         return Results.NotFound();
+     }
+
+     pizza.Name = updatePizza.Name;
+     pizza.Description = updatePizza.Description;
+     
+     await db.SaveChangesAsync();
+     return Results.NoContent();
+});
+
+app.MapDelete("/pizza/{id}", async (PizzaDb db, int id) =>
+{
+    var pizza = await db.Pizzas.FindAsync(id);
+    if (pizza is null)
+    {
+        return Results.NotFound();
+    }
+
+    db.Pizzas.Remove(pizza);
+    await db.SaveChangesAsync();
+
+    return Results.Ok();
+});
 
 app.Run();
